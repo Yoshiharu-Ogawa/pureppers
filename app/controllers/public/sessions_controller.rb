2bdd34992d92e3656d244f1f976527a0f50d8256
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-12-21 14:17:06 +0000

    [Add] sign_in view and controller

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,42 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  before_action :customer_state, only: [:create]
+
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】そのアカウントのis_deletedの中身（trueかfalse)を1件取得
+      @is_deleted = @customer.is_deleted
+      ## trueだった場合、退会しているのでサインアップ画面に遷移する
+      if @is_deleted == true
+        redirect_to 'new_registration_path'
+      end
+      ## falseだった場合、退会していないのでそのままcreateアクションを実行させる
+      if @is_deleted == false
+
+      end
+    end
+  end
+
 end
