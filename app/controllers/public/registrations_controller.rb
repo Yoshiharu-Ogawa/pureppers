ac3e5a127835eb271b022076ffbd9ee9fad33115
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-12-22 11:38:31 +0000

    [Update] about view

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,25 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_up_path_for(resource)
+      root_path
+    end
+
+  private
+
+  # def sign_up_params
+  #   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :email, :password, :password_confirmation)
+  # end
+
+  protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address,:telephone_number])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address,:telephone_number])
+  # end
+
 end
