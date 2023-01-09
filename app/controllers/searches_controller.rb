0d21e3591e79cd2d1caaa74ec175a42617ed1033
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-01-09 09:46:57 +0000

    [Add] tag search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,8 +7,10 @@ class SearchesController < ApplicationController
 
     if @range == "Customer"
       @customers = Customer.looks(params[:search], params[:word])
-    else
+    elsif @range == "Post"
       @posts = Post.looks(params[:search], params[:word])
+    else
+      @tag_posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
     end
   end
 
