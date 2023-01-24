5df49e57637812baee3bdbdacb5c3674ed872a88
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-01-24 14:56:23 +0000

    [Add] admin_comment_index

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CommentsController < ApplicationController
+  
+    # before_action :authenticate_admin!
+
+  def index
+    # @comments = Comment.page(params[:page])
+    @comments = Comment.all
+  end
+
+  def show
+    @comments = Comment.find(params[:id])
+  end
+
+  def destroy
+    # ルーティングをネストしている場合は引数2つ必要
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    comment.destroy
+    # コメント一覧に飛ぶ
+    redirect_to admin_comments_path
+  end
+
+
+
 end
