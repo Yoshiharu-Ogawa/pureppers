a0a9955bd76447cca7c6c8a690884d0fda8d300b
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-02-12 07:29:00 +0000

    [Update] comment migration

--- db/migrate/20221221122005_create_comments.rb
+++ db/migrate/20221221122005_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
 
-      t.references :customer, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :customer, type: :integer, null: false, foreign_key: true
+      t.references :post, type: :integer, null: false, foreign_key: true
 
       t.timestamps
 
--- db/schema.rb
+++ db/schema.rb
@@ -61,16 +61,6 @@ ActiveRecord::Schema.define(version: 2023_01_10_133124) do
     t.index ["post_id"], name: "index_bookmarks_on_post_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "comment_content", null: false
-    t.index ["customer_id"], name: "index_comments_on_customer_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -120,8 +110,6 @@ ActiveRecord::Schema.define(version: 2023_01_10_133124) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarks", "customers"
   add_foreign_key "bookmarks", "posts"
-  add_foreign_key "comments", "customers"
-  add_foreign_key "comments", "posts"
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
 end
