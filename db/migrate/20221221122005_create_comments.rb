74c09a0c8c25dc84a28ed08ee66ccbc136c34fc5
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-02-12 07:35:19 +0000

    [Update] comment migration

--- db/migrate/20221221122005_create_comments.rb
+++ db/migrate/20221221122005_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
 
-      t.references :customer, type: :integer, null: false, foreign_key: true
-      t.references :post, type: :integer, null: false, foreign_key: true
+      t.references :customer, type: :bigint, null: false, foreign_key: true
+      t.references :post, type: :bigint, null: false, foreign_key: true
 
       t.timestamps
 
