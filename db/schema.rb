9ecb1746e73c3ea19910dd8d991e3989b833d97e
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-02-12 13:29:49 +0000

    [update] search view

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,14 @@ ActiveRecord::Schema.define(version: 2023_01_10_133124) do
     t.index ["post_id"], name: "index_bookmarks_on_post_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "comment_content", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
