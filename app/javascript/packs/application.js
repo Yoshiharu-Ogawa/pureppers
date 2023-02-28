6518378cf18d43bd7b5e5b7af7dc0d6de6ab2494
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2023-02-28 08:04:53 +0000

    [Update] jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -14,6 +16,10 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// require("posts")
---
+++ app/javascript/packs/posts.js
@@ -0,0 +1,20 @@
+
+// $(document).ready(function() {
+//   var maxLength = 500;
+//   var content = $('#post_content');
+//   var counter = $('#counter');
+
+//   // ページの読み込み時と入力中に文字数をカウントし、文字数カウンターを更新する
+//   counter.text(maxLength + '文字まで');
+
+//   content.on('keyup input', function() {
+//     var length = $(this).val().length;
+//     var left = maxLength - length;
+
+//     if (left < 0) {
+//       left = 0;
+//     }
+
+//     counter.text(left + '文字残り');
+//   });
+// });
